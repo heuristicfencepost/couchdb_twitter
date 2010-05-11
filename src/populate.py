e90754376b1bc64f23baa4f3fbca633f07c08b46
Heuristic Fencepost <h.fencepost@gmail.com>
2010-05-10 22:55:14 -0500

    Plenty of refactoring, some enhancement of comments.

--- src/populate.py
+++ src/populate.py
@@ -2,11 +2,49 @@ from itertools import ifilterfalse
 import httplib
 import json
 
+# Use Twitter APIs at http://mike.verdone.ca/twitter/, mainly because it's
+# a good illustration of a novel yet useful interface into REST APIs
+# corresponding to a well-defined URL space.
 from twitter.api import Twitter
 
+# Populate a CouchDB instance with a set of data obtained from Twitter.  These
+# include the following:
+# - The first 100 tweets corresponding to a search term
+# - Information about the set of authors for these tweets
+# - The set of followers for each of these authors
 dbhost = "localhost"
 dbport = 5984
-conn = httplib.HTTPConnection(dbhost,dbport)
+
+# Query to use when finding tweets.
+searchquery = "#shotofjaq"
+
+# CouchDB REST API is fairly straightforward so we don't bother with any of
+# the various external Python modules for accessing them; httplib is just fine.
+def createDatabase(dbconn,dbname):
+
+    # Most straightforward approach is to just try and create the DB; if it
+    # already exists we get a 412 back (according to the CouchDB API).
+    dbconn.request("PUT","/%s/" % dbname,None)
+    r = dbconn.getresponse()
+    r.read() # body must be read in order for subsequent HTTP requests to work
+    if r.status == 412:
+        print "Database %s already exists" % dbname
+    elif r.status != 201:
+        print "Unable to create database %s: %s" % (dbname,r.reason)
+
+def createDocument(dbconn,dbname,docname,doc):
+
+    dbconn.request("PUT","/%s/%s" % (dbname,docname),json.dumps(doc))
+    r = dbconn.getresponse()
+    r.read() # body must be read in order for subsequent HTTP requests to work
+    if r.status == 409:
+        print "Version conflict detected for document %s" % (docname)
+    elif r.status == 201:
+        print "Document %s created" % docname
+    elif r.status == 202:
+        print "Document %s created" % docname
+    else:
+        print "Unable to create document %s: %s" % (docname,r.reason)
 
 # Borrowed from the itertools docs
 def unique_everseen(iterable, key=None):
@@ -24,45 +62,23 @@ def unique_everseen(iterable, key=None):
                 seen_add(k)
                 yield element
 
-def createDatabase(name):
+if __name__ == "__main__":
 
-    # Most straightforward approach is to just try and create the DB; if it
-    # already exists we get a 412 back (according to the CouchDB API).
-    conn.request("PUT","/%s/" % name,None)
-    r = conn.getresponse()
-    data = r.read()
-    if r.status == 412:
-        print "Database %s already exists" % name
-    elif r.status != 201:
-        print "Unable to create database %s: %s" % (name,r.reason)
+    search = Twitter(domain="search.twitter.com")
+    searchresults = search.search(q=searchquery,rpp=100)
+    tweets = searchresults["results"]
 
-def createDocument(database,name,doc):
+    conn = httplib.HTTPConnection(dbhost,dbport)
+    tweetdb = "tweets"
 
-    conn.request("PUT","/%s/%s" % (database,name),json.dumps(doc))
-    r = conn.getresponse()
-    data = r.read()
-    if r.status == 409:
-        print "Version conflict detected for document %s" % (name)
-    elif r.status == 201:
-        print "Document %s created" % name
-    elif r.status == 202:
-        print "Document %s created" % name
-    else:
-        print "Unable to create document %s: %s" % (name,r.reason)
+    # Make sure our database exists
+    createDatabase(conn,tweetdb)
+
+    # Create a doc in the tweet database, one for each distinct tweet
+    def createTweet(tweet):
+        createDocument(conn,tweetdb,tweet["id"],tweet)
+    map(createTweet,tweets)
 
 
-search = Twitter(domain="search.twitter.com")
-searchresults = search.search(q="#shotofjaq",rpp=100)
-tweets = searchresults["results"]
-
-# Make sure our database exists
-createDatabase("tweets")
-
-# Add some content to aforementioned database
-def createTweet(tweet):
-    createDocument("tweets",tweet["id"],tweet)
-map(createTweet,tweets)
-
-
-authors = unique_everseen([t["from_user"] for t in tweets])
-print " ".join(authors)
+    authors = unique_everseen([t["from_user"] for t in tweets])
+    print " ".join(authors)
