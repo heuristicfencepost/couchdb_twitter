a7d9cb83901fef48c0d2be1b1752ebcffa0b942d
Heuristic Fencepost <h.fencepost@gmail.com>
2010-10-04 01:21:39 -0500

    Updated some comments in order to clarify implementation details.

--- src/js/map_3a.js
+++ src/js/map_3a.js
@@ -1,12 +1,15 @@
-// Feed a sequence of user names for each tweet into the reduce function.
 function(doc) {
 
+    // Add keys for each author, keyed by their distinct Twitter ID
     if (doc.resource == "author") {
 
         emit([doc.id,"name"],doc.screen_name);
     }
     else if (doc.resource == "followers" && doc.screen_name == "damienkatz") {
 
+        // Also add keys for each follower of Damien's, again keyed by
+        // Twitter ID.  This will allow us to use CouchDB's grouping
+        // functionality to correlate author IDs to followers.
         for (var i = 0; i < doc.ids.length; ++i) {
 
             emit([doc.ids[i],'is_following'],1);
--- src/js/reduce_3a.js
+++ src/js/reduce_3a.js
@@ -1,11 +1,14 @@
 function(keys, values, rereduce) {
 
+    // Make our reduce function safe for rereduce
     if (rereduce) {
-        return values.filter(function(obj) { obj != ""; });
+        return values
     }
     else {
 
-        // Strip keys out of (key,id) tuples
+        // Strip keys out of (key,id) tuples.  Even though we're using
+        // a grouping function keys are of the form [Twitter ID, keyname]
+        // so we need to take teh second item from whatever we find.
         var truekeys = keys.map(function(arr) { return arr[0][1]; });
 
         // Build an object containing key-value pairs.  It's a bit like
@@ -14,7 +17,9 @@ function(keys, values, rereduce) {
         var foo = {};
         for (var i = 0; i < truekeys.length; ++i) { foo[truekeys[i]] = values[i]; }
 
-        if ("name" in foo && foo["is_following"] == 1) {
+        // If we have a "name" and an "is_following" value of 1 we've found
+        // an author who is also following Damien.
+        if (("name" in foo) && ("is_following" in foo) && foo["is_following"] == 1) {
 
             log("keys: " + keys + ", truekeys: " + truekeys + ", values: " + values);
             return foo["name"];
